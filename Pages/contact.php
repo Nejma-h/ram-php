cabdbc53dc1542f224c94f58fe58440dcf7f243e
Nejma-h <nejma.hammoutou@le-campus-numerique.fr>
2023-01-03 16:29:20 +0100

    Iteration 1 Etape 4

--- Pages/contact.php
+++ Pages/contact.php
@@ -1,7 +1,7 @@
 <?php include('header.php'); ?>
 <section class="container card mt-5 text-center">
     <h1 class="mt-5">Formulaire de contact</h1>
-<form action="contact.php" method="GET" class="p-5 w-auto mx-auto">
+<form action="contact.php" method="POST" class="p-5 w-auto mx-auto">
     <div class="mb-3">
 
     <select class="form-select" aria-label="Default select example" name="civilite">
@@ -31,7 +31,23 @@
     <textarea class="form-control" name="message"></textarea>
     </div>
     <div class="mb-3">
-    <button type="button" class="btn btn-dark">ENVOYER</button>
+    <button type="submit" name="submit" class="btn btn-dark">ENVOYER</button>
     </div>
 </form>
 </section>
+<?php
+$civilite = $_POST['civilite'];
+$nom = $_POST['nom'];
+$prenom = $_POST['prenom'];
+$email = $_POST['email'];
+$motif = $_POST['motif'];
+$message = $_POST['message'];
+
+$data = $civilite.' '.$nom.' '.$prenom.'<br/>'.$email.'<br/>>'.'Objet: '.$motif.'<br/>>'.$message;
+$today = date(DATE_ATOM, mktime(0, 0, 0, 7, 1, 2000));
+$file = "contact_".$today.".txt";
+
+if(isset($_POST['submit'])) {
+    file_put_contents($file, $data);
+    chmod($file,0777);
+}
